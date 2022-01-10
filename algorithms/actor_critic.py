15adf208642615b9b3180e298d4daa146121e99b
Oschart <oscar@aucegypt.edu>
2022-01-10 21:33:14 +0100

    Added new actor-critic with Q function

--- algorithms/actor_critic.py
+++ algorithms/actor_critic.py
@@ -1,4 +1,7 @@
-from .base_agent import PolicyGradientAgent
+from typing import Union
+
+from policy_models.mlp import ActorCriticMLP
+from .neural_agent import NeuralAgent
 from common import Action
 from Environment import KarelEnv
 
@@ -16,10 +19,10 @@ import pickle
 
 
 
-class ActorCritic(PolicyGradientAgent):
+class ActorCritic(NeuralAgent):
     def __init__(
         self,
-        policy,
+        policy: Union[ActorCriticMLP, str],
         env,
         GAMMA=0.99,
         learning_rate=0.0003,
@@ -35,49 +38,50 @@ class ActorCritic(PolicyGradientAgent):
             max_episodes=max_episodes,
         )
 
-    def process_episode(self, state, optimal_seq):
-        self.log_probs = []
-        self.critic_values = []
-        self.rewards = []
-        for t in range(self.max_eps_len):
-            value, policy_dist = self.policy(state)
-            value = value.detach().numpy()[0, 0]
-            dist = policy_dist.detach().numpy()
+    def compute_actor_loss(self, data):
+        s, a, r, probs = data["S"], data["A"], data["R"], data["PI"]
+        q = self.policy.Qs[0](s, a)
+        probs = torch.stack(probs)
+        #probs = self.policy(s, batch_mode=True)
+        log_pi = torch.log(probs + 1e-13)
+        Gs = self.compute_returns(r)
+        #q2_pi = ac.q2(s, a)
+        #q_pi = torch.min(q1_pi, q2_pi)
 
-            action = np.random.choice(self.num_actions, p=np.squeeze(dist))
-            optimal_action = Action.from_str[optimal_seq[t]]
-            log_prob = torch.log(policy_dist.squeeze(0)[optimal_action] + 1e-10)
-            entropy = -np.sum(np.mean(dist) * np.log(dist))
-            new_state, reward, done, _ = self.env.step(optimal_action)
+        # Entropy-regularized policy loss
+        alpha = 0.5
+        loss_pi = alpha *( -log_pi*(Gs - q)).mean()
 
-            self.rewards.append(reward)
-            self.critic_values.append(value)
-            self.log_probs.append(log_prob)
-            self.entropy_term += entropy
-            state = new_state
+        return loss_pi
+    
+    def compute_critic_loss(self, data):
+        s, a, r, s_next, d = data['S'], data['A'], data['R'], data['S_next'], data['D']
 
-            if done:
-                break
-        return t
+        q = self.policy.Qs[0](s,a)
+        alpha = 0.5
+        Gs = self.compute_returns(r)
+        
 
-    def update_policy(self):
-        # compute Q values
-        Qval = 0.0
-        Qvals = np.zeros_like(self.critic_values)
-        for t in reversed(range(len(self.rewards))):
-            Qval = self.rewards[t] + self.GAMMA * Qval
-            Qvals[t] = Qval
+        # MSE loss against Bellman backup
+        loss_q = alpha*((Gs - q).pow(2)).mean()
 
-        # update actor critic
-        critic_values = torch.FloatTensor(self.critic_values)
-        Qvals = torch.FloatTensor(Qvals)
-        log_probs = torch.stack(self.log_probs)
+        return loss_q
 
-        advantage = Qvals - critic_values
-        actor_loss = (-log_probs * advantage).mean()
-        critic_loss = 0.5 * advantage.pow(2).mean()
+    def compute_returns(self, rewards):
+        G = 0.0
+        Gs = np.zeros_like(rewards, dtype=float)
+        for t in reversed(range(len(rewards))):
+            G = rewards[t] + self.GAMMA * G
+            Gs[t] = G
+        return torch.FloatTensor(Gs)
+
+    def update_agent(self):
+        actor_loss = self.compute_actor_loss(self.epidata)
+        critic_loss = self.compute_critic_loss(self.epidata)
+
         ac_loss = actor_loss + critic_loss  # + 0.001 * self.entropy_term
 
         self.optimizer.zero_grad()
         ac_loss.backward()
         self.optimizer.step()
+        #print('well well')
