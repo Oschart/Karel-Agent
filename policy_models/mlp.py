6d39df8973d62e46986b3f828d83fc298dcf80b3
Oschart <oscar@aucegypt.edu>
2022-01-27 12:35:39 +0100

    Specified Softmax output dim to solve torch warning

--- policy_models/mlp.py
+++ policy_models/mlp.py
@@ -16,8 +16,8 @@ def combined_shape(length, shape=None):
 def mlp(sizes, activation, output_activation=nn.Identity):
     layers = []
     for j in range(len(sizes) - 1):
-        act = activation if j < len(sizes) - 2 else output_activation
-        layers += [nn.Linear(sizes[j], sizes[j + 1]), act()]
+        act = activation() if j < len(sizes) - 2 else output_activation(dim=-1)
+        layers += [nn.Linear(sizes[j], sizes[j + 1]), act]
     return nn.Sequential(*layers)
 
 
